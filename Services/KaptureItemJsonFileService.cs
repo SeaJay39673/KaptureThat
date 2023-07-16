b9eb5ad71f7f231ef7bbad274afe4a889fb8bb69
kylerlegault <kyler2001@gmail.com>
2023-07-15 22:11:18 -0600

    Re-added files

---
+++ Services/KaptureItemJsonFileService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Text.Json;
+using DynamicDisplay_ProofOfConcept.Models;
+namespace DynamicDisplay_ProofOfConcept.Services
+{
+    public class KaptureItemJsonFileService
+    {
+        private string _jsonFileName;
+        public KaptureItemJsonFileService(string jsonFileName)
+        {
+            _jsonFileName = jsonFileName;
+        }
+
+        public async Task<string> ReadFromFile()
+        {
+            using Stream filestream = await FileSystem.Current.OpenAppPackageFileAsync(_jsonFileName);
+            using StreamReader reader = new StreamReader(filestream);
+            return reader.ReadToEnd();
+        }
+
+        public ObservableCollection<KaptureBoardItemModel> GetKaptureBoardItems()
+        {
+   
+            string json = ReadFromFile().Result;
+            var content = JsonSerializer.Deserialize<ObservableCollection<KaptureBoardItemModel>>(json);
+            return content;
+        }
+    }
+}
---
+++ ViewModels/KaptureBoardViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using DynamicDisplay_ProofOfConcept.Models;
+using DynamicDisplay_ProofOfConcept.Services;
+namespace DynamicDisplay_ProofOfConcept.ViewModels
+{
+    public class KaptureBoardViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private KaptureItemJsonFileService _jsonFileService;
+        private ObservableCollection<KaptureBoardItemModel> _kaptureBoardItems;
+        public ObservableCollection<KaptureBoardItemModel> KaptureBoardItems
+        {
+            get => _kaptureBoardItems;
+            set
+            {
+                _kaptureBoardItems = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public KaptureBoardViewModel()
+        {
+            _jsonFileService = new("../Resources/Raw/TestingKaptureBoardItems.json");
+            _jsonFileService = new("TestingKaptureBoardItems.json");
+            _kaptureBoardItems = _jsonFileService.GetKaptureBoardItems();
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
