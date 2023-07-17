b667a767e7e7348f18078506daf12bfe407f58f2
Christopher J Edmunds <cedmund3@uwyo.edu>
2023-07-17 16:23:58 -0600

    feat: Add Camera View in Upload

--- Views/UploadPhoto.xaml.cs
+++ Views/UploadPhoto.xaml.cs
@@ -1,3 +1,5 @@
+using ZXing;
+
 namespace DynamicDisplay_ProofOfConcept.Views;
 
 public partial class UploadPhoto : ContentPage
@@ -5,5 +7,16 @@ public partial class UploadPhoto : ContentPage
 	public UploadPhoto()
 	{
 		InitializeComponent();
+	}
+	public void Cameras_Loaded(object sender, EventArgs e)
+	{
+		if (cameraView.NumCamerasDetected > 0)
+		{
+			cameraView.Camera = cameraView.Cameras.First();
+			MainThread.InvokeOnMainThreadAsync(async () => {
+				await cameraView.StopCameraAsync();
+				var result = await cameraView.StartCameraAsync();
+			});
+		}
 	}
 }
