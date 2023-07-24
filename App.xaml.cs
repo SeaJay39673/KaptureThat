d110bbd96fb41fb00c0d6467ad8e1156bfe45f5e
Christopher J Edmunds <cedmund3@uwyo.edu>
2023-07-24 10:23:42 -0600

    chore: removed unnecessary code

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,8 +7,4 @@ public partial class App : Application
 		InitializeComponent();
 		MainPage = new AppShell();
 	}
-    protected void OnNavigating(ShellNavigatingEventArgs args)
-    {
-		var target = args.Target;
-    }
 }
